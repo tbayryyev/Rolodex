8341437050899296a398f721186c836f77933b5e
Tahyr Bayryyev <100490172+tbayryyev@users.noreply.github.com>
2023-05-17 13:46:44 -0400

    Update Rolodex.java - removed helper function nextSep()

--- Rolodex.java
+++ Rolodex.java
@@ -1,33 +1,30 @@
 package rolodex;
+//I pledge my honor that I have abided by the Stevens Honor System.
 //Tahyr Bayryyev
 import java.util.ArrayList;
+
 
 public class Rolodex {
 	private Entry cursor;
 	private final Entry[] index;
 
 	// Constructor
+
 	Rolodex() {
-	     // an initial array to keep all 26 seperators since there are 26 letters in the alphabet
-	    index = new Entry[26];
 		
-	    // using ascii to create the different letter Separators		
+		index = new Entry[26];
+				
 	    for (int i=0; i<26; i++){
-	    	if (i == 0) {
-	    		index[i] = new Separator(null,null,(char)65);
-	    	}	
-	    	else if (i == 25) {
-	    		index[i] = new Separator(null, null,(char)90);
-	    	}else {
-	    		index[i] = new Separator(null, null,(char) (65+i));
-	    	}
+	    	index[i] = new Separator(null, null,(char) (65+i));
 	    }
-		// linking all the separators together to form a linked list
+	    
 	    for (int i = 0; i <26; i++) {
+			// when at the beginning of the list the previous pointer must be pointing to the end of the list to make it circular list
 	    	if (i == 0) {
 	    		index[i].next = index[1];
 	    		index[i].prev = index[25];
 	    	}
+			// when at the end of the list the next pointer must be pointing to the start of the list to make it circular list
 	    	else if (i == 25 ) {
 	    		index[i].next = index[0];
 	    		index[i].prev = index[24];
@@ -35,71 +32,61 @@ public class Rolodex {
 		    	index[i].next = index[i+1];
 		    	index[i].prev = index[i-1];
 	    	}
+			
 	    }
 	}
-	// helper function to get the next separator
-	private Entry nextSep(String name) {
-		int c = name.charAt(0) -65;
-		Entry nextSep;
-		
-		if (c == 25) {
-			nextSep = index[0];
-		}else {
-			nextSep = index[c+1];
+	
+	public Boolean contains(String name) {
+
+		Entry current = index[0];	
+
+		while(current.next != index[0]){
+			current = current.next;
+			if(name.equals(current.getName()) && !current.isSeparator()) {
+				return true;
 			}
-		return nextSep;
-		
+		}
+
+		return false;
+
 	}
 	
-	// goes through the linked list an determines whether the rolodex contains the given input parameter name
-	public Boolean contains(String name) {
-		int c = name.charAt(0) -65;
-		if (c>25 || c<0) {
-			return false;
-		}
-		
-		Entry current = index[c];
-		while(current != nextSep(name)){
-			if(name.equals(current.getName()) && !current.isSeparator()) {
-				break;
-			}
-			current = current.next;
-		}
-		return current != nextSep(name);
-	}
-	// returns the size of the rolodex only cards are counted, separators do not count
 	public int size() {
 		int count = 0;
 		Entry current = index[0];
 		
 		while(current.next!= index[0]) {
+			current = current.next;
 			if(!current.isSeparator()) {
 				count++;
 			}
-			current = current.next;
 		}
+		
 		return count;
+		
 	}
 	
-	//returns an ArrayList with all the cellphones of name (in the order in which they occur in the Rolodex)
-	//If name is not in the Rolodex, then an IllegalArgumentException with the message "lookup: name not found" is thrown
 	public ArrayList<String> lookup(String name) {
 		if(!contains(name)) {
 	    	throw new IllegalArgumentException("lookup: name not found");
 	    }
+	    
 		ArrayList<String> l = new ArrayList<String>();
-		int c = name.charAt(0) - 65;
-		Entry current = index[c];
+		
+						
+		Entry current = index[0];
 		    
-		while(current != nextSep(name)) {
+		while(current.next != index[0]) {
+			current = current.next;
 		    if(name.equals(current.getName()) && !current.isSeparator()) {
 		    	l.add(Card.class.cast(current).getCell());
+		    	
 		    	}
-		    current = current.next;
 		    }
 		return l;
 	}
-	// toString method to visualize the rolodex 
+
+
 	public String toString() {
 		Entry current = index[0];
 
@@ -111,35 +98,42 @@ public class Rolodex {
 		b.append(current.toString()+"\n");		
 		return b.toString();
 	}
-	
-	// method adds a new card with the specified infor-mation to the Rolodex 
-	// If the card already exists (with the same name and cell), then a IllegalArgumentException with the message "addCard: duplicate entry" is thrown
+
+
+
+
 	public void addCard(String name, String cell) {
-		int c = name.charAt(0) -65;
 		
-		if (c>25 || c<0 || !name.matches("[a-zA-Z]+")) {
+		if (!name.matches("[a-zA-Z]+")) {
 			throw new IllegalArgumentException("addCard: Invalid name entry");
 		}
 		
 		if(!cell.matches("[0-9]+")) {
 			throw new IllegalArgumentException("addCard: Invalid cell entry");
 		}
-	
+		
 		if (contains(name) && lookup(name).contains(cell)) {
 			throw new IllegalArgumentException("addCard: duplicate entry");
 		}
-	
-		Entry current = index[c];
 		
-		while(current != nextSep(name)) {
+
+		Entry current = index[0];
+		
+		while(current.next != index[0]) {
+
 			if (current.next.isSeparator() && name.compareTo(current.getName()) >=0 ) {
-				Card C = new Card(current, current.next,name,cell);	
+				Card C = new Card(current, current.next,name,cell);
+					
 				Entry n = current.next;
+				
+					
 				current.next = C;
 				n.prev = C;
 				break;
 					
 				}
+				
+			
 			if (name.compareTo(current.getName()) >=0 && name.compareTo(current.next.getName()) <= 0) {
 				Card C = new Card(current, current.next,name,cell);
 				
@@ -154,9 +148,7 @@ public class Rolodex {
 		}
 		
 	}
-	//removes the specified card
-	//throws an IllegalArgumentException with message "removeCard: name does not exist" if there is not card for that name
-	// If there is a card with that name but with a different cell-phone number, it throws an IllegalArgumentException with the message "removeCard: cell for that name does not exist"
+		
 	public void removeCard(String name, String cell) {
 		
 		if(!contains(name)) {
@@ -166,13 +158,12 @@ public class Rolodex {
 		if(!lookup(name).contains(cell)) {
 			throw new IllegalArgumentException("removeCard: cell for that name does not exist");
 		}
+				
+		Entry current = index[0];
 		
-		int c = name.charAt(0) -65;
+		while(current.next != index[0]) {
+			current = current.next;
 
-		
-		Entry current = index[c];
-		
-		while(current != nextSep(name)) {
 			if (name.equals(current.getName()) && !current.isSeparator() && cell.equals(Card.class.cast(current).getCell())){
 				
 				Entry p = current.prev;
@@ -181,13 +172,10 @@ public class Rolodex {
 				p.next = n;
 				n.prev = p;
 			}
-			current = current.next;
+			
 		}
-
 	}
 	
-	//removes all cards for name
-	//Throws anIllegalArgumentException with message "removeAllCards: name does not exist" if the name is not in the Rolodex
 	public void removeAllCards(String name) {
 		if(!contains(name)) {
 			throw new IllegalArgumentException("removeAllCards: name does not exist");
@@ -202,25 +190,32 @@ public class Rolodex {
 	}
 
 	// Cursor operations
-	
-	// sets the cursor field to the separator for “A”
+
 	public void initializeCursor() {
 		cursor = index[0];
+
 	}
-	//moves cursor to the next separator
+
 	public void nextSeparator() {
+		
 		cursor = cursor.next;
+		
 		while(!cursor.isSeparator()) {
+			
 			cursor = cursor.next;
+			
 		}
+
 	}
-	//moves cursor to the next entry, which could be card or a separator
+
 	public void nextEntry() {
 		cursor = cursor.next;
+
 	}
-	//returns the string representation of the current entry pointed to by the cursor
+
 	public String currentEntryToString() {
 		return cursor.toString();
+
 	}
 
 }
